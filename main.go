427dbb19d287ff01e6c935476ab23e923372ea1f
Eduardo M. Santos <160991364+supitsdu@users.noreply.github.com>
2024-05-30 13:49:50 -0300

    feat: support reading from stdin if no file path or direct text is provided (#1)
    This update adds flexibility to the clipper tool by allowing it to read input from stdin if neither a file path nor direct text is provided.
    
    Changes:
    - Added support for reading from stdin when no file path or direct text is provided.
    - Updated usage instructions to reflect the new behavior.
    
    This enhancement enables users to use clipper more intuitively in pipelines or scenarios where reading from stdin is preferred.

--- main.go
+++ main.go
@@ -1,36 +1,50 @@
 package main
 
 import (
-    "fmt"
-    "io/ioutil"
-    "os"
-    "github.com/atotto/clipboard" // Import the clipboard library
+	"flag"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/atotto/clipboard"
 )
 
 func main() {
-    // Check if the correct number of arguments are provided
-    if len(os.Args) != 2 {
-        fmt.Println("Usage: clipper <file_path>")
-        os.Exit(1)
-    }
+	// Define the flag for copying text directly from command line argument
+	directText := flag.String("c", "", "Copy text directly from command line argument")
+	flag.Parse()
 
-    // Get the file path from command line arguments
-    filePath := os.Args[1]
+	var contentStr string
 
-    // Read the content of the file
-    content, err := ioutil.ReadFile(filePath)
-    if err != nil {
-        fmt.Printf("Error reading file: %v\n", err)
-        os.Exit(1)
-    }
+	if *directText != "" {
+		// Use the provided direct text
+		contentStr = *directText
+	} else if len(flag.Args()) == 1 {
+		// Read the content from the file path provided
+		filePath := flag.Arg(0)
+		content, err := os.ReadFile(filePath)
+		if err != nil {
+			fmt.Printf("Error reading file '%s': %v\n", filePath, err)
+			os.Exit(1)
+		}
+		contentStr = string(content)
+	} else {
+		// Read from stdin
+		input, err := io.ReadAll(os.Stdin)
+		if err != nil {
+			fmt.Printf("Error reading from stdin: %v\n", err)
+			os.Exit(1)
+		}
+		contentStr = string(input)
+	}
 
-    // Write the content to the clipboard
-    err = clipboard.WriteAll(string(content))
-    if err != nil {
-        fmt.Printf("Error copying content to clipboard: %v\n", err)
-        os.Exit(1)
-    }
+	// Write the content to the clipboard
+	err := clipboard.WriteAll(contentStr)
+	if err != nil {
+		fmt.Printf("Error copying content to clipboard: %v\n", err)
+		os.Exit(1)
+	}
 
-    // Print success message
-    fmt.Println("Clipboard updated with file content. Ready to paste!")
+	// Print success message
+	fmt.Println("Clipboard updated successfully. Ready to paste!")
 }
