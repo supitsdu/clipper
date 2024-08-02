8d0abaf3ca787b21d5c1e93a6163cf9a09a07278
Eduardo M. Santos <160991364+supitsdu@users.noreply.github.com>
2024-08-02 18:24:45 -0300

    chore: improve ErrFileNotFound message

--- cli/console/errors.go
+++ cli/console/errors.go
@@ -3,7 +3,7 @@ package console
 import "errors"
 
 var (
-	ErrFileNotFound       = errors.New("file does not exist or can't be accessed")
+	ErrFileNotFound       = errors.New("file does not exist or cannot be accessed")
 	ErrReadingDirectories = errors.New("reading from directories is not currently supported")
 	ErrPermissionDenied   = errors.New("permission denied")
 )
